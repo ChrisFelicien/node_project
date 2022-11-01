50bed6a85bda5f8565a95d72aff0eb1f3b3ee29b
chrisfelicien <f.ilungakalenga@gmail.com>
2022-11-01 13:26:26 +0100

    Refarctoring controller adding async wrapper to avoid try catch

--- 2.Task_Manager/starter/controllers/task.js
+++ 2.Task_Manager/starter/controllers/task.js
@@ -1,123 +1,79 @@
 const Task = require("../Model/task");
+const asyncWrapper = require("./../middlewares/asyncWrapper");
 
-exports.getAllTaks = async (req, res) => {
-  try {
-    const tasks = await Task.find();
-    res.status(200).json({
-      status: "Success",
-      result: tasks.length,
-      data: {
-        tasks,
-      },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      message: {
-        error,
-      },
+exports.getAllTaks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find();
+  res.status(200).json({
+    status: "Success",
+    result: tasks.length,
+    data: {
+      tasks,
+    },
+  });
+});
+
+exports.getTask = asyncWrapper(async (req, res) => {
+  const { id: taskId } = req.params;
+  const task = await Task.findById(taskId);
+
+  if (!task) {
+    return res.status(404).json({
+      status: "Fail",
+      message: `No task wi the ID: ${taskId}`,
     });
   }
-};
 
-exports.getTask = async (req, res) => {
-  try {
-    const { id: taskId } = req.params;
-    const task = await Task.findById(taskId);
+  res.status(200).json({
+    status: "success",
+    data: {
+      task,
+    },
+  });
+});
 
-    if (!task) {
-      return res.status(404).json({
-        status: "Fail",
-        message: `No task wi the ID: ${taskId}`,
-      });
-    }
+exports.createTaks = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.json({
+    status: "success",
+    data: {
+      task,
+    },
+  });
+});
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        task,
-      },
+exports.updataTask = asyncWrapper(async (req, res) => {
+  const taskId = req.params.id;
+  const task = await Task.findByIdAndUpdate(taskId, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!task)
+    return res.status(404).json({
+      status: "Fail",
+      message: `No task wi the ID: ${taskId}`,
     });
-  } catch (error) {
-    res.status(400).json({
+
+  return res.status(200).json({
+    status: "success",
+    data: {
+      task,
+    },
+  });
+});
+
+exports.deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskId } = req.params;
+  const task = await Task.findByIdAndDelete(taskId);
+
+  if (!task) {
+    return res.status(500).json({
       status: "fail",
-      message: {
-        error,
-      },
+      message: `No task with ID: ${taskId}`,
     });
   }
-};
-
-exports.createTaks = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.json({
-      status: "success",
-      data: {
-        task,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "fail",
-      message: {
-        error,
-      },
-    });
-  }
-};
-
-exports.updataTask = async (req, res) => {
-  try {
-    const taskId = req.params.id;
-    const task = await Task.findByIdAndUpdate(taskId, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!task)
-      return res.status(404).json({
-        status: "Fail",
-        message: `No task wi the ID: ${taskId}`,
-      });
-
-    return res.status(200).json({
-      status: "success",
-      data: {
-        task,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "fail",
-      message: {
-        error,
-      },
-    });
-  }
-};
-
-exports.deleteTask = async (req, res) => {
-  try {
-    const { id: taskId } = req.params;
-    const task = await Task.findByIdAndDelete(taskId);
-
-    if (!task) {
-      return res.status(500).json({
-        status: "fail",
-        message: `No task with ID: ${taskId}`,
-      });
-    }
-    return res.status(202).json({
-      status: "success",
-      message: "The task has been deleted",
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "fail",
-      message: {
-        error,
-      },
-    });
-  }
-};
+  return res.status(202).json({
+    status: "success",
+    message: "The task has been deleted",
+  });
+});
