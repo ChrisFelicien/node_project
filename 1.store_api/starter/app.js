b29def4b1d9d05e4b3acf4cd30287b646200785b
chrisfelicien <f.ilungakalenga@gmail.com>
2022-11-07 12:50:19 +0100

    set up configuration

--- 1.store_api/starter/app.js
+++ 1.store_api/starter/app.js
@@ -1,7 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./utils/connectDB");
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const startServer = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`The server is runnig on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
